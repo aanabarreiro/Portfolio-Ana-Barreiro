489b9ae57242e1e489a1cbb66978bb81c7850ac1
Ana  Beatriz Barreiro <167429974+aanabarreiro@users.noreply.github.com>
2024-08-21 14:34:38 -0300

    Arquivo pronto para entrega.
    Arquivos revisados e finalizados.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-document.getElementById('mostraAside').addEventListener('click', function() {
-    const aside = document.getElementById('myAside');
-    let section= document.getElementById('sec')
-    
-    if (aside.style.display === 'none') {
-      aside.style.display = 'block';
-      section.style.display = 'none'
-    } else {
-      aside.style.display = 'none';
-      section.style.display= 'block';
-    }
-  });
-  
+document.getElementById("mostraAside").addEventListener("click", function () {
+  const aside = document.getElementById("myAside");
+  let section = document.getElementById("sec");
+
+  if (aside.style.display === "none" || aside.style.display === "") {
+    aside.style.display = "block";
+    section.style.display = "none";
+  } else {
+    aside.style.display = "none";
+    section.style.display = "block";
+  }
+});
